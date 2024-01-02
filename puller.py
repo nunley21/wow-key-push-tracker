480ada0f63c0ebba698b394a3bffa522bdfc6382
nunley21 <nunley1994@gmail.com>
2024-01-02 07:46:26 -0500

    Update

--- puller.py
+++ puller.py
@@ -2,7 +2,7 @@ import requests
 import json
 import datetime
 def get_key(team, level):
-    r = requests.get(f'https://raider.io/api/teams/mythic-plus/dungeon-runs?season=season-df-2&team={team}&region=us&dungeon={level}', timeout=30)
+    r = requests.get(f'https://raider.io/api/teams/mythic-plus/dungeon-runs?season=season-df-3&team={team}&region=us&dungeon={level}', timeout=30)
     text = r.json()
     return text["viewTeamMythicPlusDungeonRunsApi"]["runs"]
 
@@ -32,23 +32,42 @@ def main():
             team = team.strip("\n")
             dungeon = dungeon.strip("\n")
             data = get_key(team, dungeon)
-            keys_store = []
+            keys_store_fort = []
+            keys_store_tryan = []
             for key in data:
-
-                keys_store.append(key['summary']['mythic_level'])
-                # print(f"{team}: {dungeon}: key: {key['summary']['mythic_level']}")
+                if key['summary']['status'] == "finished":
+                    if key['summary']["weekly_modifiers"][0]["name"] == "Tyrannical":
+                        keys_store_tryan.append(key['summary']['mythic_level'])
+                    elif key['summary']["weekly_modifiers"][0]["name"] == "Fortified":
+                        keys_store_fort.append(key['summary']['mythic_level'])
             try:
-                max_key = max(keys_store)
+                max_key_fort = max(keys_store_fort)
+            except:
+                max_key_fort = 0
+            try:
+                max_key_tryan = max(keys_store_tryan)
+            except:
+                max_key_tryan = 0
+            try:
+                top_keys = {"Fortified": {}, "Tyrannical": {}}
                 for key in data:
-                    if max_key == key['summary']['mythic_level']:
+                    if max_key_fort == key['summary']['mythic_level'] and key['summary']["weekly_modifiers"][0]["name"] == "Fortified":
                         key_time = round((key['summary']["clear_time_ms"] / key['summary']["keystone_time_ms"]) * 100)
                         plus = get_chest_plus(key['summary']['num_chests'])
-                        top[key['summary']['dungeon']['name']] = {"key_level":key['summary']['mythic_level'], "percent": key_time, "plus": plus}
-            except:
-                pass
-            keys_store = []
+                        top_keys["Fortified"] = {"key_level": key['summary']['mythic_level'], "percent": key_time, "plus": plus}
+                    if max_key_tryan == key['summary']['mythic_level'] and key['summary']["weekly_modifiers"][0]["name"] == "Tyrannical":
+                        key_time = round((key['summary']["clear_time_ms"] / key['summary']["keystone_time_ms"]) * 100)
+                        plus = get_chest_plus(key['summary']['num_chests'])
+                        top_keys["Tyrannical"] = {"key_level": key['summary']['mythic_level'], "percent": key_time, "plus": plus}
+                    top[key['summary']['dungeon']['name']] = top_keys
+            except Exception as e:
+                print(e)
+
 
         team_store[team] = top
+
+
+
     return team_store
 
 
@@ -57,6 +76,16 @@ if __name__ == '__main__':
     data = main()
     json.dump(data, open('score.json', 'w'), sort_keys=True, indent=4, separators=(',', ': '))
 
-
-
-
+#     max_key = max(keys_store)
+#     for key in data:
+#         if max_key == key['summary']['mythic_level'] and key['summary']["weekly_modifiers"][0][
+#             "name"] == ability_toughness:
+#             key_time = round((key['summary']["clear_time_ms"] / key['summary']["keystone_time_ms"]) * 100)
+#             plus = get_chest_plus(key['summary']['num_chests'])
+#             top[key_name][ability_toughness] = {
+#                 ability_toughness: {"key_level": key['summary']['mythic_level'], "percent": key_time, "plus": plus,
+#                                     "ability": ability_toughness}}
+#             print(top)
+# except Exception as e:
+# print(e)
+# pass
--- static/main.js
+++ static/main.js
@@ -1,13 +1,9 @@
-
-
 $.getJSON('/api/get_all', function(data) {
 
+    console.log(data);
+    buildTables(data)
 
 
-    for (let team in data) {
-        generateTable(team, Object.keys(data[team]).length, data[team])
-
-    }
     document.getElementById("spinner").style.display = "none";
 
     console.log("done")
@@ -15,107 +11,121 @@ $.getJSON('/api/get_all', function(data) {
 
 });
 
+function buildTables(data) {
+
+    for (const groupName in data) {
+        const groupData = data[groupName];
+        let jumbo = document.createElement("div")
+        jumbo.classList.add('container', 'mt-3')
+        let jumbo_inner = document.createElement("div");
+        jumbo_inner.classList.add('mt-4', 'p-5', 'bg-dark', 'text-white', 'rounded')
+        let team_h1 = document.createElement("h1")
+        let team = groupName.replaceAll('-', ' ')
+        const words = team.split(" ");
+
+        for (let i = 0; i < words.length; i++) {
+            words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+        }
+        let team_upper = words.join(" ");
+        team_h1.innerText = team_upper
+        jumbo_inner.appendChild(team_h1)
+
+        const groupTable = document.createElement('table');
+        groupTable.classList.add('table', 'table-striped', 'table-bordered', 'table-primary', 'text-center')
+        groupTable.innerHTML = `
+      <thead>
+        <tr>
+          <th>Affix</th>
+          <th>Atal'Dazar</th>
+          <th>Black Rook Hold</th>
+          <th>DOTI: Galakrond's Fall</th>
+          <th>DOTI: Murozond's Rise</th>
+          <th>Darkheart Thicket</th>
+          <th>The Everbloom</th>
+          <th>Throne of the Tides</th>
+          <th>Waycrest Manor</th>
+       
+        </tr>
+      </thead>
+      <tbody>
+    `;
+
+        // Create an array to store dungeon names
+
+        let fort_row = document.createElement("tr");
+        let tyran_row = document.createElement("tr");
+
+        let fortcell = document.createElement("td");
+        let fortcellText = document.createTextNode('Fortified');
+        fortcell.appendChild(fortcellText);
+        fort_row.appendChild(fortcell);
+
+        let Tyrannicalcell = document.createElement("td");
+        const TyrannicalcellText = document.createTextNode('Tyrannical');
+        Tyrannicalcell.appendChild(TyrannicalcellText);
+        tyran_row.appendChild(Tyrannicalcell);
+        for (const dungeonName in groupData) {
+            const dungeon = groupData[dungeonName]
+
+            // row.appendChild()
+            const cell = document.createElement("td");
 
 
-function generateTable(team, tlengh, data) {
-    console.log(data[1])
-  // creates a <table> element and a <tbody> element
-  const tbl = document.createElement("table");
-  tbl.classList.add('table', 'table-striped','table-bordered', 'table-primary', 'text-center');
-  const tblBody = document.createElement("tbody");
 
-  // creating all cells
-  for (let i = 0; i < 3; i++) {
-    console.log(i)
-    // creates a table row
-    const row = document.createElement("tr");
-    if (i === 0){
+            let fortcellText
+            if (typeof dungeon.Fortified.key_level === 'undefined') {
+                fortcellText = document.createTextNode(` `);
+            } else {
+                fortcellText = document.createTextNode(`${dungeon.Fortified.key_level} ${dungeon.Fortified.plus}`);
+            }
 
-    for (let key in data) {
-      // Create a <td> element and a text node, make the text
-      // node the contents of the <td>, and put the <td> at
-      // the end of the table row
-      const cell = document.createElement("th");
-      const cellText = document.createTextNode(`${key}`);
-      cell.appendChild(cellText);
-      row.appendChild(cell);
+            cell.appendChild(fortcellText);
+            fort_row.appendChild(cell);
+            groupTable.querySelector('tbody').appendChild(fort_row);
+
+
+            const tyrancell = document.createElement("td");
+            let tyrancellText
+            if (typeof dungeon.Tyrannical.key_level === 'undefined') {
+                tyrancellText = document.createTextNode(` `);
+            } else {
+                tyrancellText = document.createTextNode(`${dungeon.Tyrannical.key_level} ${dungeon.Tyrannical.plus}`);
+            }
+
+            tyrancell.appendChild(tyrancellText);
+            tyran_row.appendChild(tyrancell);
+            groupTable.querySelector('tbody').appendChild(tyran_row);
+
+        }
+        const total_row = document.createElement("tr");
+        const total_cell = document.createElement("td");
+        total_cell.setAttribute("colspan", 9)
+        let top_tyran_keys = []
+        let top_fort_keys = []
+        for (const dungeonName in groupData) {
+            const dungeon = groupData[dungeonName]
+            top_tyran_keys.push(dungeon.Tyrannical.key_level)
+            top_fort_keys.push(dungeon.Fortified.key_level)
+        }
+        let top_3_fort_keys = top_fort_keys.sort((a, b) => b - a).slice(0, 3)
+        let top_3_tyran_keys = top_tyran_keys.sort((a, b) => b - a).slice(0, 3)
+        let fort_sum = top_3_fort_keys.reduce((partialSum, a) => partialSum + a, 0)
+        let tyran_sum = top_3_tyran_keys.reduce((partialSum, a) => partialSum + a, 0)
+
+        let total_cell_text = document.createTextNode(fort_sum + tyran_sum)
+        total_cell.appendChild(total_cell_text);
+        total_row.appendChild(total_cell);
+        groupTable.querySelector('tbody').appendChild(total_row);
+
+
+
+        groupTable.innerHTML += `
+      </tbody>
+    `
+
+
+        jumbo_inner.appendChild(groupTable)
+        jumbo.appendChild(jumbo_inner)
+        document.body.appendChild(jumbo);
     }
-
-    // add the row to the end of the table body
-    tblBody.appendChild(row);
-    } else if (i === 1) {
-      for (let key in data) {
-          // Create a <td> element and a text node, make the text
-          // node the contents of the <td>, and put the <td> at
-          // the end of the table row
-          const cell = document.createElement("td");
-          const cellText = document.createTextNode(`${data[key].key_level} ${data[key].plus}`);
-          cell.appendChild(cellText);
-          row.appendChild(cell);
-      }
-    } else {
-        for (let key in data) {
-                  // Create a <td> element and a text node, make the text
-          // node the contents of the <td>, and put the <td> at
-          // the end of the table row
-          const cell = document.createElement("td");
-          const cellText = document.createTextNode(`${data[key].percent}%`);
-          if (data[key].percent > 100){
-              cell.classList.add("table-danger")
-          } else {
-              cell.classList.add("table-success")
-          }
-          cell.appendChild(cellText);
-          row.appendChild(cell);
-
-    }
-    }
-
-    // add the row to the end of the table body
-    tblBody.appendChild(row);
-
-
-
-
-  }
-  const total_row = document.createElement("tr");
-  const total_cell = document.createElement("td");
-  total_cell.setAttribute("colspan", Object.keys(data).length)
-  let keys = []
-  for (let key in data) {
-      keys.push(data[key].key_level)
-  }
-  let top_keys = keys.sort((a,b) => b-a).slice(0,5)
-    console.log(top_keys)
-
-  let total_cell_text = document.createTextNode(top_keys.reduce((partialSum, a) => partialSum + a, 0))
-  total_cell.appendChild(total_cell_text);
-  total_row.appendChild(total_cell);
-  tblBody.appendChild(total_row);
-
-
-
-  // put the <tbody> in the <table>
-  tbl.appendChild(tblBody);
-  // appends <table> into <body>
-  let  jumbo = document.createElement("div")
-  jumbo.classList.add('container', 'mt-3')
-  let jumbo_inner =   document.createElement("div");
-  jumbo_inner.classList.add('mt-4', 'p-5', 'bg-dark', 'text-white', 'rounded')
-  let team_h1 = document.createElement("h1")
-  team = team.replaceAll('-', ' ')
-  const words = team.split(" ");
-
-  for (let i = 0; i < words.length; i++) {
-      words[i] = words[i][0].toUpperCase() + words[i].substr(1);
-  }
-  let team_upper = words.join(" ");
-  team_h1.innerText = team_upper
-  jumbo_inner.appendChild(team_h1)
-  jumbo_inner.appendChild(tbl)
-  jumbo.appendChild(jumbo_inner)
-  document.body.appendChild(jumbo);
-
-
-
 }
